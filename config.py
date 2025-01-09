17f8971082198cc2f80341f0ba6f2a98f2719d0c
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2025-01-09 21:04:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHIVANSH474")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/veda_ki_vani")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+oKV6PitSA7FhZTM9")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
